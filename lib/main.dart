e14b620953eb7f699de720b9b523039b6385c0b1
sarabiji <118169526+sarabiji@users.noreply.github.com>
2023-11-26 18:34:17 +0530

    the app is now scrollable

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class InfoPage extends StatelessWidget {
       //appbar specs
       appBar: AppBar(
         title: Text(
-          'INFORMATION',
+          'INTRODUCTION',
           style: TextStyle(
               color: Colors.grey[700],
               fontSize: 30,
@@ -30,130 +30,132 @@ class InfoPage extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.fromLTRB(30, 45, 30, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Center(
-                child: CircleAvatar(
-                  backgroundImage: AssetImage("assets/piano.jpeg"),
-                  radius: 70,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage("assets/piano.jpeg"),
+                    radius: 70,
+                  ),
                 ),
-              ),
 
-              const Divider(
-                height: 50,
-                color: Colors.amber,
-              ),
-              Text(
-                'NAME',
-                style: TextStyle(
-                  color: Colors.amberAccent[700],
-                  letterSpacing: 2,
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
+                const Divider(
+                  height: 50,
+                  color: Colors.amber,
                 ),
-              ),
-              Text(
-                'Sara Biji',
-                style: TextStyle(
-                    color: Colors.grey[700], letterSpacing: 4, fontSize: 14),
-              ),
-              const SizedBox(height: 5),
-              Text(
-                'ABOUT ME',
-                style: TextStyle(
-                  color: Colors.amberAccent[700],
-                  letterSpacing: 2,
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
+                Text(
+                  'NAME',
+                  style: TextStyle(
+                    color: Colors.amberAccent[700],
+                    letterSpacing: 2,
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Text(
-                "Enthusiastic young developer proficient in Python, C, and Java, specializing in MySQL for database management. Outside of coding, I find joy in sketching, reading, and exploring psychology. Balancing logic and creativity, I'm on a journey to weave innovative solutions while embracing the diverse facets of life.",
-                style: TextStyle(
-                  color: Colors.grey[700],
-                  letterSpacing: 2,
+                Text(
+                  'Sara Biji',
+                  style: TextStyle(
+                      color: Colors.grey[700], letterSpacing: 4, fontSize: 14),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                'LAPTOP STATS',
-                style: TextStyle(
-                  color: Colors.amberAccent[700],
-                  letterSpacing: 2,
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
+                const SizedBox(height: 5),
+                Text(
+                  'ABOUT ME',
+                  style: TextStyle(
+                    color: Colors.amberAccent[700],
+                    letterSpacing: 2,
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              RichText(
-                  text: TextSpan(
-                      text:
-                          'Device name: LAPTOP-T8822H8K\nProcessor:	Intel(R) Core(TM) i7-10510U CPU @ 1.80GHz   2.30 GHz \nInstalled RAM:	8.00 GB (7.84 GB usable)\nSystem type: 	64-bit operating system, x64-based processor\t',
-                      style: TextStyle(
-                        color: Colors.grey[700],
-                        letterSpacing: 2,
-                      ))),
-              const SizedBox(
-                height: 20,
-              ),
+                Text(
+                  "Enthusiastic young developer proficient in Python, C, and Java, specializing in MySQL for database management. Outside of coding, I find joy in sketching, reading, and exploring psychology. Balancing logic and creativity, I'm on a journey to weave innovative solutions while embracing the diverse facets of life.",
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    letterSpacing: 2,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'LAPTOP STATS',
+                  style: TextStyle(
+                    color: Colors.amberAccent[700],
+                    letterSpacing: 2,
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                RichText(
+                    text: TextSpan(
+                        text:
+                            'Device name: LAPTOP-T8822H8K\nProcessor:	Intel(R) Core(TM) i7-10510U CPU @ 1.80GHz   2.30 GHz \nInstalled RAM:	8.00 GB (7.84 GB usable)\nSystem type: 	64-bit operating system, x64-based processor\t',
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          letterSpacing: 2,
+                        ))),
+                const SizedBox(
+                  height: 20,
+                ),
 
-              //inside the column i want a row with mail icon and the text of the email address
-              const Divider(
-                height: 4,
-                color: Colors.amber,
-              ),
-              const SizedBox(height: 6),
-              Row(
-                children: [
-                  Icon(
-                    Icons.mail,
-                    color: Colors.grey[700],
-                  ),
-                  const SizedBox(width: 10),
-                  Text("abcdefghijk@gmail.com",
-                      style: TextStyle(
-                        color: Colors.grey[700],
-                        letterSpacing: 1,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      )),
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.call,
-                    color: Colors.grey[700],
-                  ),
-                  const SizedBox(width: 10),
-                  Text("+91 1234567890",
-                      style: TextStyle(
-                        color: Colors.grey[700],
-                        letterSpacing: 1,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      )),
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.laptop,
-                    color: Colors.grey[700],
-                  ),
-                  const SizedBox(width: 10),
-                  Text(
-                    "LAPTOP-T8822H8K",
-                    style: TextStyle(
+                //inside the column i want a row with mail icon and the text of the email address
+                const Divider(
+                  height: 4,
+                  color: Colors.amber,
+                ),
+                const SizedBox(height: 6),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.mail,
                       color: Colors.grey[700],
-                      letterSpacing: 1,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    const SizedBox(width: 10),
+                    Text("abcdefghijk@gmail.com",
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          letterSpacing: 1,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.call,
+                      color: Colors.grey[700],
+                    ),
+                    const SizedBox(width: 10),
+                    Text("+91 1234567890",
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          letterSpacing: 1,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.laptop,
+                      color: Colors.grey[700],
+                    ),
+                    const SizedBox(width: 10),
+                    Text(
+                      "LAPTOP-T8822H8K",
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                        letterSpacing: 1,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
